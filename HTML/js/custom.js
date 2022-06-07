72923f9ff26db1bceb2a0586e51f9d86b8374a3c
Nông Nghiệp QC <vanthouit@gmail.com>
2022-06-07 16:55:42 +0700

    udpdate index.html

---
+++ HTML/js/custom.js
@@ -0,0 +1,22 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+var swiper = new Swiper("#product-category", {
+    slidesPerView: 5,
+    spaceBetween: 10,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
